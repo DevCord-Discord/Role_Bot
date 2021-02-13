086a303d0c8e40be067993407a0599aab06322d6
l3th4l <safalyaglobal@gmail.com>
2021-02-13 21:36:44 +0530

    m

--- roles.py
+++ roles.py
@@ -153,6 +153,7 @@ class Roles(commands.Cog):
         else:
             
             await ctx.message.delete()
+            print(r_channel_id, type(r_channel_id))
             ch = self.bot.get_channel(r_channel_id)
             await ctx.channel.send("Please go to {} to use that command".format(ch.mention))
 
